8b189012a89efde9f3d065e0e17739727d5e4672
Denis <tapfereherz@gmail.com>
2018-09-22 18:26:15 +0700

    Select Data base - выбор и подключение к базе данных

---
+++ SelectDataBase/src/main/java/SelectDataBase.java
@@ -0,0 +1,46 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SelectDataBase {
+    // jdbc драйвер и ссылка на базу данных
+    static final String JDBC_DRIVER = " com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/newbigbase?useSSL=false";
+
+    //доступ к базе данных
+    public static final String USER = "root";
+    public static final String  PASS = "rjirf_ljcz";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+
+
+        try{
+            //регистрируем jdbc драйвер
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            //открываем соединение
+            System.out.println(" Соединение с базой данных ...");
+            conn = DriverManager.getConnection(DB_URL, USER , PASS);
+            System.out.println(" Соединение с базой данных успешно установлено");
+
+        } catch (SQLException e) {
+            System.out.println("исключение");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try{
+                if(conn != null)
+                    conn.close();
+
+            }
+            catch (SQLException se){
+                se.printStackTrace();
+            }
+        }
+        System.out.println("База закрыта");
+    }
+}
